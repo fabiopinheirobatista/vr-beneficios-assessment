13a3f33ec0410706b6067a5a05c64cb942aa746d
Fabio <fabiobatista.bertioga@gmail.com>
2025-09-08 21:49:09 -0300

    First commit

---
+++ src/main/java/vr_backend_assessment/ApiAssessmentApplication.java
@@ -0,0 +1,13 @@
+package vr_backend_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vr_backend_assessment/ApiAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package vr_backend_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
